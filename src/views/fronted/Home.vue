7e00031bf777bed62a3f602ed623d814262f2882
李晏瑋 <magic556679@gmail.com>
2022-12-26 22:47:16 +0800

    add prettier

--- src/views/fronted/Home.vue
+++ src/views/fronted/Home.vue
@@ -1,12 +1,11 @@
 <template>
   <div>
     <h3 class="text-red-500">home</h3>
-            <input type="text" class="border border-collapse">
-                <Test></Test>
+    <input type="text" class="border border-collapse" />
+    <img src="" alt="" />
+    <Test></Test>
   </div>
-          </template>
+</template>
 <script setup lang="ts">
-import Test from './Test.vue'
-const a = '1'
-console.log(a)
+import Test from './Test.vue';
 </script>
