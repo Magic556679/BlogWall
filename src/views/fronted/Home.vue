c79a2e6025812212160f9ebb68f0ba70b5e99cf4
李晏瑋 <magic556679@gmail.com>
2022-12-28 21:43:21 +0800

    add 別名設定

--- src/views/fronted/Home.vue
+++ src/views/fronted/Home.vue
@@ -2,10 +2,8 @@
   <div>
     <h3 class="text-red-500">home</h3>
     <input type="text" class="border border-collapse" />
-    <img src="" alt="" />
-    <Test></Test>
   </div>
 </template>
 <script setup lang="ts">
-import Test from './Test.vue';
+
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,11 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import eslintPlugin from 'vite-plugin-eslint'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [vue(), eslintPlugin({ cache: false })],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': path.resolve(__dirname, './src'),
     }
-  },
-  plugins: [vue(), eslintPlugin({ cache: false })]
+  }
 })
