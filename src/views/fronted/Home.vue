fae63c816fcbb9b94a2bc7c869636ab7d05ece7a
李晏瑋 <magic556679@gmail.com>
2023-02-02 21:59:52 +0800

    feat add axios

--- src/views/fronted/Home.vue
+++ src/views/fronted/Home.vue
@@ -1,9 +1,46 @@
 <template>
   <div>
-    <h3 class="text-red-500">home</h3>
-    <input type="text" class="border border-collapse" />
+    <input
+      type="text"
+      class="border-blue-500 border-2 mt-2"
+      v-model="firstName"
+    />
+    <input
+      type="text"
+      class="border-blue-500 border-2 mt-2"
+      v-model="lastName"
+    />
+    <input
+      type="text"
+      class="border-blue-500 border-2 mt-2"
+      v-model="computedName"
+    />
   </div>
 </template>
 <script setup lang="ts">
-
+import axios from 'axios';
+import { ref, computed } from 'vue';
+const api = import.meta.env.VITE_SERVER_BASE_API;
+const apiData = ref([]);
+axios
+  .get(`${api}/posts`)
+  .then((res) => {
+    const {
+      data: { data },
+    } = res;
+    apiData.value = data;
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+const firstName = ref('Li');
+const lastName = ref('tony');
+const computedName = computed({
+  get() {
+    return firstName.value + ' ' + lastName.value;
+  },
+  set(newValue) {
+    [firstName.value, lastName.value] = newValue.split(' ');
+  },
+});
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import eslintPlugin from 'vite-plugin-eslint'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig({
   plugins: [vue(), eslintPlugin({ cache: false })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    }
-  }
-})
+    },
+  },
+});
