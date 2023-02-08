b1ea6e508130d9dbfa0eb3b7b6071a3617cda51e
李晏瑋 <magic556679@gmail.com>
2023-02-08 10:35:13 +0800

    feat login page is changed router-view

---
+++ src/views/fronted/login/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex align-middle items-center min-h-screen">
+    <main
+      class="flex mx-auto w-[870px] border-black border-2 border-solid px-[48px] py-[96px]"
+    >
+      <div class="w-1/2">
+        <img :src="loginImage" />
+      </div>
+      <div class="w-1/2">
+        <router-view></router-view>
+      </div>
+    </main>
+  </div>
+</template>
+<script setup lang="ts">
+import loginImage from '@/assets/images/login.jpg';
+</script>
---
+++ src/views/fronted/login/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1 class="text-4xl font-bold text-center">Welcome BlogWall</h1>
+  <input
+    type="text"
+    placeholder="Email"
+    class="w-full h-[50px] mt-9 px-6 py-4 border-2 border-solid border-black"
+  />
+  <input
+    type="text"
+    placeholder="Password"
+    class="w-full h-[50px] mt-4 px-6 py-4 border-2 border-solid border-black"
+  />
+  <button
+    type="button"
+    class="w-full mt-9 py-4 border-2 border-solid border-black text-white bg-[#03438D] shadow-"
+  >
+    登入
+  </button>
+  <p class="mt-4 text-base text-center">註冊帳號</p>
+</template>
