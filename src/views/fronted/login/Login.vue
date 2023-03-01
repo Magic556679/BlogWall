1169efcf37ab248f9f4de560691830da20561e97
李晏瑋 <magic556679@gmail.com>
2023-03-01 22:59:06 +0800

    feat add login function

--- src/views/fronted/login/Login.vue
+++ src/views/fronted/login/Login.vue
@@ -1,7 +1,8 @@
 <template>
-  <VeeForm fef="form" v-slot="{ errors }" @submit="login">
+  <!-- @submit="login" -->
+  <VeeForm fef="form" v-slot="{ errors }">
     <Field
-      v-model="formData.mail"
+      v-model="formData.email"
       name="email"
       type="email"
       placeholder="Email"
@@ -10,31 +11,65 @@
       :class="{ 'border-red-500': errors['email'] }"
     />
     <ErrorMessage name="email" class="text-red-500" />
+    <!-- <Field
+      v-model="formData.password"
+      name="password"
+      type="password"
+      placeholder="Password"
+      :rules="rulesPassword"
+      class="w-full h-[50px] mt-4 px-6 py-4 border-2 border-solid border-black"
+      :class="{ 'border-red-500': errors['password'] }"
+    />
+    <ErrorMessage name="password" class="text-red-500" /> -->
     <input
-      type="text"
+      v-model="formData.password"
+      name="password"
+      type="password"
       placeholder="Password"
       class="w-full h-[50px] mt-4 px-6 py-4 border-2 border-solid border-black"
     />
+    <p class="text-red-500">{{ errorMessage }}</p>
     <button
-      type="submit"
+      @click="login"
+      type="button"
       class="w-full mt-9 py-4 border-2 border-solid border-black rounded-lg text-white bg-[#03438D] shadow-3xl"
     >
       登入
     </button>
   </VeeForm>
-  <p class="mt-4 text-base text-center">  <router-link
+  <p class="mt-4 text-base text-center">
+    <router-link
       :to="{
         name: 'register',
       }"
       >註冊帳號</router-link
-    ></p>
-
+    >
+  </p>
 </template>
 <script setup lang="ts">
 import { ref } from 'vue';
+import { login as loginApi } from '@/services/api/user';
+import { AxiosError } from 'axios';
 
-const formData = ref({ mail: '' });
-function login() {
-  console.log('submit');
-}
+const formData = ref({ email: '', password: '' });
+const errorMessage = ref('');
+
+const login = async () => {
+  try {
+    const { data } = await loginApi(formData.value);
+    errorMessage.value = ''
+    console.log(data);
+  } catch (error: unknown ) {
+    if (error instanceof AxiosError) {
+      errorMessage.value = error.response?.data?.message;
+    }
+    console.error(error)
+  }
+};
+// const rulesPassword = (value: string): string | boolean => {
+//   if (value) {
+//     return true;
+//   }
+//   return '不得為空';
+// };
 </script>
