b9871b929d8f5569a068d6185fcad96d614c2cee
李晏瑋 <magic556679@gmail.com>
2023-03-01 22:57:09 +0800

    feat add server proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import eslintPlugin from 'vite-plugin-eslint';
@@ -8,6 +8,16 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        // target: 'http://127.0.0.1:3010/',
+        target: loadEnv('', process.cwd()).VITE_SERVER_BASE_API,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
     },
   },
 });
