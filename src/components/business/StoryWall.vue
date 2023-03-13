3e7964478efe7960f3c0713071b9211dbc882f4d
李晏瑋 <magic556679@gmail.com>
2023-03-13 19:51:28 +0800

    style add border rounded

--- src/components/business/StoryWall.vue
+++ src/components/business/StoryWall.vue
@@ -1,10 +1,10 @@
 <template>
   <div
-    class="mt-4 p-6 border-2 border-black"
+    class="mt-4 p-6 border-2 border-black rounded-lg"
     v-for="item in data"
     :key="item._id"
   >
-    <div class="flex">
+    <section class="flex">
       <img
         class="w-[45px] h-[45px] mr-4 border-2 border-black rounded-full inline-block"
         :src="userDefault"
@@ -14,13 +14,54 @@
         <p>tony</p>
         <span>2023/03/08</span>
       </div>
-    </div>
-    <div class="mt-4">
+    </section>
+    <section class="mt-4">
       <div>
         {{ item.content }}
       </div>
-      <img :src="demo" alt="" />
-    </div>
+      <img class="mt-4" :src="demo" alt="" />
+      <i class="fa-regular fa-heart mt-5 mr-2"></i>
+      <span>4444個讚</span>
+    </section>
+    <section class="flex mt-4 items-center">
+      <img
+        class="w-[45px] h-[45px] mr-4 border-2 border-black rounded-full inline-block"
+        :src="userDefault"
+        alt=""
+      />
+      <input
+        type="text"
+        placeholder="請留言"
+        class="grow h-10 px-4 rounded-l-lg border-l-2 border-t-2 border-b-2 border-black"
+      />
+      <button
+        type="button"
+        class="h-10 px-2 rounded-r-lg border-2 border-solid border-black text-white bg-[#03438D]"
+      >
+        留言
+      </button>
+    </section>
+    <section class="flex px-4 py-[18px]">
+      <img
+        class="w-[45px] h-[45px] mr-4 border-2 border-black rounded-full inline-block"
+        :src="userDefault"
+        alt=""
+      />
+      <div>
+        <div>
+          <p>路人甲</p>
+          <span>2023/03/08</span>
+        </div>
+        <div>
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+            delectus assumenda tenetur reprehenderit eveniet possimus nulla
+            quasi voluptate consequatur cumque, architecto, excepturi nihil,
+            sequi corrupti doloremque necessitatibus totam! In, corporis?
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 <script setup lang="ts">
@@ -38,5 +79,5 @@ const props = defineProps({
     required: true,
   },
 });
-console.log(props)
+console.log(props);
 </script>
--- src/views/fronted/Home.vue
+++ src/views/fronted/Home.vue
@@ -4,7 +4,7 @@
       <div class="flex justify-between">
         <select
           name="最新貼文"
-          class="mr-3 border-2 border-solid border-black w-[156px] h-11"
+          class="mr-3 rounded-lg border-2 border-solid border-black w-[156px] h-11"
         >
           <option value="最新貼文">最新貼文</option>
           <option value="2">2</option>
@@ -12,7 +12,7 @@
         <div class="relative grow">
           <input
             type="text"
-            class="p-2 pl-8 w-full border-2 border-solid border-black focus:bg-white focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:border-transparent"
+            class="p-2 pl-8 w-full rounded-lg border-2 border-solid border-black focus:bg-white focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:border-transparent"
             placeholder="search..."
             value=""
           />
@@ -36,7 +36,7 @@
         <StoryWallContent :data="postsData"></StoryWallContent>
       </main>
     </div>
-    <div class="sm:w-[30%] px-5 py-8 border-2 border-black">
+    <div class="sm:w-[30%] px-5 py-8 border-2 rounded-lg border-black">
       <button
         type="button"
         class="w-full py-4 border-2 border-solid border-black rounded-lg text-white bg-[#03438D] shadow-3xl"
