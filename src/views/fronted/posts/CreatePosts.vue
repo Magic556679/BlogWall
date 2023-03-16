47ec2358d23670373dd8f5e564de49e8c209ccfb
李晏瑋 <magic556679@gmail.com>
2023-03-16 23:11:33 +0800

    style 貼文切版

---
+++ src/views/fronted/posts/CreatePosts.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex justify-between mt-12">
+    <section class="sm:w-[65%]">
+      <div class="w-full py-4 rounded-lg border-2 border-solid border-black">
+        <h1 class="text-xl font-medium text-center">張貼動態</h1>
+      </div>
+      <div class="mt-3 p-8 rounded-lg border-2 border-solid border-black">
+        <h3 class="text-base">貼文內容</h3>
+        <textarea
+          v-model="comment"
+          name="w3review"
+          rows="4"
+          cols="50"
+          class="mt-1 px-4 py-3 rounded-lg border-2 border-solid border-black"
+        >
+        </textarea>
+      </div>
+      <div>
+        <Button size="w-[128px]"></Button>
+      </div>
+    </section>
+    <Aside></Aside>
+  </div>
+</template>
+<script setup lang="ts">
+import Aside from '@/components/layout/Aside.vue';
+import Button from '@/components/common/button.vue'
+import { ref } from 'vue';
+const comment = ref('hello');
+
+</script>
