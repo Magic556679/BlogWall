212a854a114a4cb6a01d3d26389db8e3a9051284
李晏瑋 <magic556679@gmail.com>
2023-03-16 23:12:12 +0800

    style 移除側邊欄

--- src/components/business/StoryWall.vue
+++ src/components/business/StoryWall.vue
@@ -1,67 +1,72 @@
 <template>
-  <div
-    class="mt-4 p-6 border-2 border-black rounded-lg"
+  <div v-if="data.length !== 0">
+    <div
     v-for="item in data"
     :key="item._id"
-  >
-    <section class="flex">
-      <img
-        class="w-[45px] h-[45px] mr-4 border-2 border-black rounded-full inline-block"
-        :src="userDefault"
-        alt=""
-      />
-      <div>
-        <p>tony</p>
-        <span>2023/03/08</span>
-      </div>
-    </section>
-    <section class="mt-4">
-      <div>
-        {{ item.content }}
-      </div>
-      <img class="mt-4" :src="demo" alt="" />
-      <i class="fa-regular fa-heart mt-5 mr-2"></i>
-      <span>4444個讚</span>
-    </section>
-    <section class="flex mt-4 items-center">
-      <img
-        class="w-[45px] h-[45px] mr-4 border-2 border-black rounded-full inline-block"
-        :src="userDefault"
-        alt=""
-      />
-      <input
-        type="text"
-        placeholder="請留言"
-        class="grow h-10 px-4 rounded-l-lg border-l-2 border-t-2 border-b-2 border-black"
-      />
-      <button
-        type="button"
-        class="h-10 px-2 rounded-r-lg border-2 border-solid border-black text-white bg-[#03438D]"
-      >
-        留言
-      </button>
-    </section>
-    <section class="flex px-4 py-[18px]">
-      <img
-        class="w-[45px] h-[45px] mr-4 border-2 border-black rounded-full inline-block"
-        :src="userDefault"
-        alt=""
-      />
-      <div>
+    class="mt-4 p-6 border-2 border-black rounded-lg"
+    >
+      <section class="flex">
+        <img
+          class="w-[45px] h-[45px] mr-4 border-2 border-black rounded-full inline-block"
+          :src="userDefault"
+          alt=""
+        />
         <div>
-          <p>路人甲</p>
+          <p>tony</p>
           <span>2023/03/08</span>
         </div>
+      </section>
+      <section class="mt-4">
+        <div>
+          {{ item.content }}
+        </div>
+        <img class="mt-4" :src="demo" alt="" />
+        <i class="fa-regular fa-heart mt-5 mr-2"></i>
+        <span>4444個讚</span>
+      </section>
+      <section class="flex mt-4 items-center">
+        <img
+          class="w-[45px] h-[45px] mr-4 border-2 border-black rounded-full inline-block"
+          :src="userDefault"
+          alt=""
+        />
+        <input
+          type="text"
+          placeholder="請留言"
+          class="grow h-10 px-4 rounded-l-lg border-l-2 border-t-2 border-b-2 border-black"
+        />
+        <button
+          type="button"
+          class="h-10 px-2 rounded-r-lg border-2 border-solid border-black text-white bg-[#03438D]"
+        >
+          留言
+        </button>
+      </section>
+      <section class="flex px-4 py-[18px]">
+        <img
+          class="w-[45px] h-[45px] mr-4 border-2 border-black rounded-full inline-block"
+          :src="userDefault"
+          alt=""
+        />
         <div>
           <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
-            delectus assumenda tenetur reprehenderit eveniet possimus nulla
-            quasi voluptate consequatur cumque, architecto, excepturi nihil,
-            sequi corrupti doloremque necessitatibus totam! In, corporis?
+            <p>路人甲</p>
+            <span>2023/03/08</span>
+          </div>
+          <div>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+              delectus assumenda tenetur reprehenderit eveniet possimus nulla
+              quasi voluptate consequatur cumque, architecto, excepturi nihil,
+              sequi corrupti doloremque necessitatibus totam! In, corporis?
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
+  </div>
+  <div v-else class="mt-4 p-6 border-2 border-black rounded-lg">
+    <p>目前尚無動態，新增一則貼文吧！</p>
   </div>
 </template>
 <script setup lang="ts">
