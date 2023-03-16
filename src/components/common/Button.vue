87bbc726484666559f281ea3bea5fb9cd1b48e6f
李晏瑋 <magic556679@gmail.com>
2023-03-16 23:10:49 +0800

    feat button aside 元件化拆分

---
+++ src/components/common/Button.vue
@@ -0,0 +1,18 @@
+<template>
+    <button
+      :class="props.size"
+      type="button"
+      class="py-4 border-2 border-solid border-black rounded-lg text-white bg-[#03438D] shadow-3xl"
+    >
+      張貼動態
+    </button>
+</template>
+<script setup lang="ts">
+const props = defineProps({
+  size: {
+    type: String,
+    default: 'w-full'
+  }
+})
+
+</script>
---
+++ src/components/layout/Aside.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :class="props.width" class="px-5 py-8 h-full border-2 rounded-lg border-black">
+    <button
+      type="button"
+      class="w-full py-4 border-2 border-solid border-black rounded-lg text-white bg-[#03438D] shadow-3xl"
+    >
+      張貼動態
+    </button>
+    <div class="flex mt-6">
+      <img
+        class="w-[45px] h-[45px] mr-4 border-2 border-black rounded-full inline-block"
+        :src="profilePictureDefault"
+        alt=""
+      />
+      <p>tony</p>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import profilePictureDefault from '@/assets/images/userDefault.jpg';
+const props = defineProps({
+  width: {
+    type: String,
+    default: 'sm:w-[30%]'
+  }
+})
+
+
+</script>
