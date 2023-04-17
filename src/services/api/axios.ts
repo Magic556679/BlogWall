9c6c28070b6704df7a1b03a91c95aba915b67c81
李晏瑋 <magic556679@gmail.com>
2023-04-17 22:55:28 +0800

    feat axios 攔截器增加 withCredentials: true 處理跨域問題、cookie 增加 Bearer token

--- src/services/api/axios.ts
+++ src/services/api/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const service = axios.create({
+  withCredentials: true,
   baseURL: import.meta.env.VITE_SERVER_BASE_API,
   headers: {
     accept: 'application/json',
@@ -8,8 +9,16 @@ const service = axios.create({
   timeout: 30000,
 });
 
+
 service.interceptors.request.use(
   (config) => {
+    const getCookieTokenName: string | undefined = document.cookie.split("; ").find(item => item.includes('id_token='))
+    const token: string | undefined = getCookieTokenName?.split('=')[1]
+    if (token) {
+      config.headers = {
+        Authorization: `Bearer ${token}`
+      };
+    };
     return config;
   },
   (error) => {
