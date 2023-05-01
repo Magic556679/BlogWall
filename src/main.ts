a1cea92dcfb1ef1d09b9044d0752920a6f73ae88
李晏瑋 <magic556679@gmail.com>
2023-05-01 20:49:35 +0800

    install pinia

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import { Form, Field, ErrorMessage, defineRule, configure } from 'vee-validate'
 import AllRules from '@vee-validate/rules'
 import zh from '@vee-validate/i18n/dist/locale/zh_TW.json'
@@ -17,9 +18,12 @@ configure({
   validateOnInput: true, // 當輸入任何內容直接進行驗證
 });
 setLocale('zh');
+const pinia = createPinia()
 const app = createApp(App)
+
 app.component('VeeForm', Form)
 app.component('Field', Field)
 app.component('ErrorMessage', ErrorMessage)
 app.use(router)
+app.use(pinia)
 app.mount('#app')
