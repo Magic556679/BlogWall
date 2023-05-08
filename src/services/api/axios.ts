3175b048ddf8706502d35914ea97d80dc6f0031d
李晏瑋 <magic556679@gmail.com>
2023-05-08 22:25:50 +0800

    style 暫時註解 withCredentials, 排版

--- src/services/api/axios.ts
+++ src/services/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const service = axios.create({
-  withCredentials: true,
+  // withCredentials: true,
   baseURL: import.meta.env.VITE_SERVER_BASE_API,
   headers: {
     accept: 'application/json',
@@ -9,16 +9,17 @@ const service = axios.create({
   timeout: 30000,
 });
 
-
 service.interceptors.request.use(
   (config) => {
-    const getCookieTokenName: string | undefined = document.cookie.split("; ").find(item => item.includes('id_token='))
-    const token: string | undefined = getCookieTokenName?.split('=')[1]
+    const getCookieTokenName: string | undefined = document.cookie
+      .split('; ')
+      .find((item) => item.includes('id_token='));
+    const token: string | undefined = getCookieTokenName?.split('=')[1];
     if (token) {
       config.headers = {
-        Authorization: `Bearer ${token}`
+        Authorization: `Bearer ${token}`,
       };
-    };
+    }
     return config;
   },
   (error) => {
