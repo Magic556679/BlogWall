ba6045573d88564396a81ec71efaa52b93e60aa4
李晏瑋 <magic556679@gmail.com>
2023-05-08 22:27:20 +0800

    feat 修改 npm run dev port localhost 避免 imgur url 403

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,9 @@ export default defineConfig({
     },
   },
   server: {
+    host: '0.0.0.0',
+    port: 8080,
+    open: true,
     proxy: {
       '/api': {
         // target: 'http://127.0.0.1:3010/',
