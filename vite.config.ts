24d4c6f967cea05f23becda3558cf8d8609062b6
李晏瑋 <magic556679@gmail.com>
2023-10-05 17:39:18 +0800

    build test

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig({
   base: '/BlogWall/',
-  build: { outDir: 'dist' },
+  build: { outDir: 'docs' },
   plugins: [vue(), eslintPlugin({ cache: false })],
   resolve: {
     alias: {
